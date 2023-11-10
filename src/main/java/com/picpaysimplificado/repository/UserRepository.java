85d326b5e0a4a59802a1e97c3b0542603bfcad77
Matheus Marques <matheuspadilha.dev@gmail.com>
2023-11-10 19:26:34 -0300

    feat: - create Transaction class - create UserRepository - create application.properties

---
+++ src/main/java/com/picpaysimplificado/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.picpaysimplificado.repository;
+
+import com.picpaysimplificado.model.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findUserByDocument(String document);
+
+    Optional<User> findUserById(Long id);
+}
