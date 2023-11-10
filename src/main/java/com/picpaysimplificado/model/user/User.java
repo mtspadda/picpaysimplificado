cb8a8de41d51405c975749396473cb2e60938ffc
Matheus Marques <matheuspadilha.dev@gmail.com>
2023-11-10 19:25:40 -0300

    feat: created User and UserType

---
+++ src/main/java/com/picpaysimplificado/model/user/User.java
@@ -0,0 +1,43 @@
+package com.picpaysimplificado.model.user;
+
+import com.fasterxml.jackson.databind.annotation.EnumNaming;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.usertype.UserType;
+
+import java.math.BigDecimal;
+
+@Entity(name="user")
+@Table(name="user")
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode(of="id")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    @Column(unique = true)
+    private String document;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    private BigDecimal balance;
+
+    @Enumerated(EnumType.STRING)
+    private UserType userType;
+
+
+}
